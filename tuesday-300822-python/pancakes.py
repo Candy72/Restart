7df770ae67f26c14782c364eced11498fe464cc5
Candy72 <mmcandy@pm.me>
2022-09-03 16:17:09 +1200

    adding pancake

---
+++ tuesday-300822-python/pancakes.py
@@ -0,0 +1,8 @@
+#puppo
+    reply = input("Do you want to know how to cook pancakes? y/n ")
+if reply == "y":
+    print("recipe")
+elif reply == "n":
+    print("program quit")
+else:
+    print("reply out of range")
