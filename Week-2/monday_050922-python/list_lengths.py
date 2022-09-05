f4d299f6932a22942ca5757973cdf08e6476c889
Candy72 <mmcandy@pm.me>
2022-09-05 15:19:18 +1200

    wk2 monday

---
+++ Week-2/monday_050922-python/list_lengths.py
@@ -0,0 +1,26 @@
+food = ['honey', 'jam', 3, 6, 6, 22, "peanut butter"]  #changed my_list to food
+list_length = len(food)
+# index = 0 
+
+for blargh in food:   # changed item to blargh
+    print(food.pop()) #() to run the function
+ 
+
+
+
+# while True:
+#     item = my_list[index]
+#     print(item)
+#     index = index + 1
+
+# while list_length > 0:
+# while len(my_list) > 0:
+    # list_length = len(my_list)  - if you put the list_length here it will put you out by 1
+#     item = my_list.pop(0)
+#     print(item)
+#     # list_length = len(my_list)
+# print("Done")
+
+
+##Stack - LIFO - last in first out  - Firsthink of a stack of dishes - always taking an item of the end  last one on the stack first one off the stack
+#Queue - FIFO - First in First out -  one thing in one thing out - one thing then the next
