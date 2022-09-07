1250e61d2e9cdd760b06c349ceef9b5f8ec9f654
Candy72 <mmcandy@pm.me>
2022-09-07 15:18:55 +1200

    wk2_3 final part

--- Week-2/wk2_2_cardb.py
+++ Week-2/wk2_2_cardb.py
@@ -2,12 +2,24 @@
 # asking using for input
 #stays in memory while the program is running 
 
-#1. Asking user for input on make of car
-car_make = input("what is the make of the car ")
+#0. Loop this
+while True: 
 
-#2. Ask for type of car
-car_model = input("What is the car model: ")
+    #1. Asking user for input on make of car
+    car_make = input("what is the make of the car: ")
 
-#2. Ask for colour of car
-car_colour = input("What is the colour of the car: ")
+    #2. Ask for type of car
+    car_model = input("What is the car model: ")
 
+    #2. Ask for colour of car
+    car_color = input("What is the color of the car: ")
+
+    #4. put into dictionary
+
+    car = {
+     "make": car_make,
+     "model": car_model,
+     "car color": car_color
+    }
+    print(id(car)) # prints where it is in memory
+    print(car) #shift tabe = unindent
