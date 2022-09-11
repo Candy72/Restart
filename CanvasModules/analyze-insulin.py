017b9930444638e92288cbc2c97f4aefff948c28
Candy72 <mmcandy@pm.me>
2022-09-12 10:17:05 +1200

    module exercises 1 - 12

---
+++ CanvasModules/analyze-insulin.py
@@ -0,0 +1,4 @@
+# Exercise 1: Retrieving the protein sequence of human preproinsulin
+
+# Exercise 2: Obtaining the protein sequence of human insulin
+
---
+++ CanvasModules/caesar-cipher.py
@@ -0,0 +1,9 @@
+# Python3.6  
+# Coding: utf-8  
+# Exercise 1: Creating a user-defined function
+
+def getDoubleAlphabet(alphabet):
+    doubleAlphabet = alphabet + alphabet
+    return doubleAlphabet
+
+
